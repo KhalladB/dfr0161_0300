737610455bc2a91c5ea2f66f976a403f8f2a6021
KhalladB <eng.khallad@hotmail.com>
2016-11-17 10:17:18 +0300

    Update openag_dfr0161.h
    Rearranged the code, & removed unnecessary comments

--- openag_dfr0161.h
+++ openag_dfr0161.h
@@ -19,35 +19,20 @@ class Dfr0161: public Module {
   void update();
   bool get_water_potential_hydrogen(std_msgs::Float32 &msg);
   
-  // From dfr0161_0300_sensor
-  float averageArray(int* arr, int number);
-  
-  // const int ph_pin = 1;
-
-  // From dfr0161_0300_sensor
-  //float _ph_raw;
-  //float _ph_filtered;
   
   private:
+  // Private Functions
   float getData();
-  //bool readSensor();
+  float averageArray(int* arr, int number);
   
   // Private Variables
-  int _ph_pin;// =  ph_pin;
+  int _ph_pin;
+  float _ph_calibration_coefficient;
+  float _ph_calibration_offset;
   float _water_potential_hydrogen;
   bool _send_water_potential_hydrogen;
   uint32_t _time_of_last_reading;
   const uint32_t _min_update_interval = 2500;
-  
-  // From dfr0161_0300_sensor
-  //String _ph_instruction_code;
-  //int _ph_instruction_id;
-  float _ph_calibration_coefficient;
-  float _ph_calibration_offset;
-  
-  
-
-
 };
 
 #endif
