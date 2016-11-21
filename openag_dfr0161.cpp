a580e3cdc0a1b9a505852ad32f94301bf50a368b
KhalladB <eng.khallad@hotmail.com>
2016-11-21 12:18:50 +0300

    Update openag_dfr0161.cpp

--- openag_dfr0161.cpp
+++ openag_dfr0161.cpp
@@ -28,7 +28,6 @@
    _ph_calibration_coefficient = 3.5;
    _ph_calibration_offset = -0.1;
    pinMode(_ph_pin, INPUT);    //declare _ph_pin as an input
-   Serial.begin(9600);
  }
  
  void Dfr0161::update(){
